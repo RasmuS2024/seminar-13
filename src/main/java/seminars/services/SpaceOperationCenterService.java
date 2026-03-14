ed7e30660248501e87a8725f7f881337782b8c70
RasmuS2024 <rasburkhanov@yandex.ru>
2026-03-14 12:58:45 +0300

    created new SpaceOperationCenterService

---
+++ src/main/java/seminars/services/SpaceOperationCenterService.java
@@ -0,0 +1,11 @@
+package seminars.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpaceOperationCenterService {
+    public void addSatellite(AddSatelliteRequest addSatelliteRequest) {
+
+    }
+
+}
